d2bcbc96234951032b03645406e855d8d2dbd50d
AhmedAlyEl-Ghannam <89432827+AhmedAlyEl-Ghannam@users.noreply.github.com>
2023-05-12 13:21:10 +0300

    Updated PSD + added comments + pass bitrate

--- MultiLevel3.m
+++ MultiLevel3.m
@@ -1,10 +1,9 @@
-function MultiLevel3(r)
-    % Assuming a bitrate of one
-    T = length(r) / 1;
-    plot_precision = 200;
-    N = plot_precision*length(r);
-    dt = T/N;
-    t = 0:dt:length(r);
+function MultiLevel3(r, bitrate)
+    T = length(r) / bitrate; % bits / (bits/sec) = time in sec
+    plot_precision = 100; % samples/bit
+    N = plot_precision*length(r); % samples/bit * bits = samples
+    dt = T/N; % time/sample (time for each sample)
+    t = 0:dt:T;
     x = zeros(1,length(t)); % output signal
     
     % Storing the first element
@@ -41,16 +40,19 @@ function MultiLevel3(r)
         end
     end
     
-    [Pxx,F] = periodogram(x);
-    
     figure
     subplot(2,1,1);  
     plot(t,x)
-    title('Multi-level 3')
-    axis([0 length(r) -1.5 1.5]);
+    title('MLT-3')
+    axis([0 (length(r)/bitrate) -1.5 1.5]);
+    xlabel("Time (s)");
+    ylabel("Voltage (V)");
+    
+    [psdx, freq] = CalculatePSD(x, dt);
     
     subplot(2,1,2);
-    plot(F,10*log10(Pxx))
-    title('Multi-level 3 PSD')
-    axis([0 3.5 -80 30]);
+    plot(freq,pow2db(psdx));
+    title('MLT-3 PSD');
+    xlabel("Frequency (Hz)");
+    ylabel("Power/Frequency (dB/Hz)");
 end
